82c0d85f3f79d53fe6b5ac5fbbd07c495105fff1
lucas lamar <lucass.lamar@gmail.com>
2019-04-29 13:29:03 -0300

    Codico do motor de passo

---
+++ motor-de-passo/motor-de-passo.ino
@@ -0,0 +1,21 @@
+#include <Stepper.h> 
+ 
+const int stepsPerRevolution = 500; 
+  
+//Inicializa a biblioteca utilizando as portas de 8 a 11 para 
+//ligacao ao motor 
+Stepper myStepper(stepsPerRevolution, 8,10,9,11); 
+  
+void setup() 
+{ 
+ //Determina a velocidade inicial do motor 
+ myStepper.setSpeed(60);
+} 
+  
+void loop() 
+{ 
+
+ //Gira o motor no sentido anti-horario a 120 graus
+ myStepper.step(900); 
+
+}
